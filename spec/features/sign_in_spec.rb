51eb4f54295207df579fe2e36753a67bd128aab9
Nicolas Berrizbeitia <nbr625@gmail.com>
2015-10-08 23:58:08 -0400

    changed some of the matchers

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -3,32 +3,31 @@ require 'selenium-webdriver'
 feature "Signing in to ArtWare" do
 
   before(:all) do
-    @driver = Selenium::WebDriver.for(:chrome)
-    @driver.navigate.to 'https://artwear.herokuapp.com/'
-    
-  end
-  before(:each) do
-    click_button 'Logout'
-    visit 'users/sign_in'
-  end
-
-  it 'should let me in with a valid account' do  
-    fill_in 'user[email]', :with => 'Murat@breakthrough.com'
-    fill_in 'user[password]', :with => 'asdfasdf'
-    click_button "Sign in"
-    expect(page).to have_content("Artist of the Month")
-  end
-
-  it 'should not let me in with an invalid account' do
-    fill_in 'user[email]', :with => 'a@a.com'
-    fill_in 'user[password]', :with => 'asdfasdf'
-    click_button "Sign in"
-    expect(page).to have_content("Invalid email or password")
+    @driver = :chrome
   end
 
   after(:all) do
     @driver.quit
   end
+
+  it 'should let me in with a valid account' do
+    @driver.get 'https://artwear.herokuapp.com/'
+    find("#signin").click  
+    fill_in 'Email :', :with => 'Murat@breakthrough.com'
+    fill_in 'Password :', :with => 'asdfasdf'
+    find("#signin").click
+    expect(page).to have_content("Artist of the Month")
+  end
+
+  it 'should not let me in with an invalid account' do
+    @driver.get 'https://artwear.herokuapp.com/'
+    fill_in 'Email :', :with => 'a@a.com'
+    fill_in 'Password :', :with => 'asdfasdf'
+    find("#signin").click
+    expect(page).to have_content("Invalid email or password")
+  end
+
+
 
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,3 +6,6 @@ Capybara.default_driver = :selenium
 Capybara.app_host = 'http://artwear.herokuapp.com/'
 Capybara.run_server = false
 
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+end
