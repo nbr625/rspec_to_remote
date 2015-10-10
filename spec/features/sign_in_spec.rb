7c5d0f25c1e0560510b782b4315dc2961ab1f401
Nicolas Berrizbeitia <nbr625@gmail.com>
2015-10-10 16:44:01 -0400

    finally got sign_in method to pass. The damb has cracked now just pass the logic to the remaining 9 features

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -3,31 +3,51 @@ require 'selenium-webdriver'
 feature "Signing in to ArtWare" do
 
   before(:all) do
-    @driver = :chrome
+    @driver = Selenium::WebDriver.for(:firefox)
+    @driver.navigate.to 'https://artwear.herokuapp.com/users/sign_in'
   end
+
+
+
+  it 'should let user in with a valid account and password' do
+    @driver.find_element(:id, 'emailfield').send_keys 'murat@breakthrough.com'
+    @driver.find_element(:id, 'passwordfield').send_keys 'asdfasdf'
+    @driver.find_element(:id, 'submit').click
+    notice = @driver.find_element(:id, 'notice').text
+    expect(notice).to eq('Signed in successfully.')
+  end
+
+  it 'should let user logout' do
+    @driver.find_element(:id, 'logoutpath').click
+    sleep(inspection_time=5)
+    notice = @driver.find_element(:id, 'notice').text
+    expect(notice).to eq('Signed out successfully.')
+
+  end
+
+
+  it 'should not let use in with an invalid account' do
+    @driver.navigate.to 'https://artwear.herokuapp.com/users/sign_in'
+    @driver.find_element(:id, 'emailfield').send_keys 'invalid@breakthrough.com'
+    @driver.find_element(:id, 'passwordfield').send_keys 'asdfasdf'
+    @driver.find_element(:id, 'submit').click
+    sleep(inspection_time=3)
+    alert = @driver.find_element(:id, 'alert').text
+    expect(alert).to eq("Invalid email or password.")
+  end
+
+  it 'should not let user in with an invalid password' do
+    @driver.find_element(:id, 'emailfield').send_keys 'Murat@breakthrough.com'
+    @driver.find_element(:id, "passwordfield").send_keys 'incorrect'
+    @driver.find_element(:id, "submit").click
+    sleep(inspection_time=3)
+    alert = @driver.find_element(:id, 'alert').text
+    expect(alert).to eq("Invalid email or password.")
+  end
+
 
   after(:all) do
     @driver.quit
   end
-
-  it 'should let me in with a valid account' do
-    @driver.get 'https://artwear.herokuapp.com/'
-    find("#signin").click  
-    fill_in 'Email :', :with => 'Murat@breakthrough.com'
-    fill_in 'Password :', :with => 'asdfasdf'
-    find("#signin").click
-    expect(page).to have_content("Artist of the Month")
-  end
-
-  it 'should not let me in with an invalid account' do
-    @driver.get 'https://artwear.herokuapp.com/'
-    fill_in 'Email :', :with => 'a@a.com'
-    fill_in 'Password :', :with => 'asdfasdf'
-    find("#signin").click
-    expect(page).to have_content("Invalid email or password")
-  end
-
-
-
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,14 @@
 require 'capybara/rspec'
 require 'rubygems'
 require 'bundler/setup'
+require 'rspec/expectations'
+require 'selenium-webdriver'
 
 Capybara.default_driver = :selenium
 Capybara.app_host = 'http://artwear.herokuapp.com/'
 Capybara.run_server = false
 
 Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+  Capybara::Selenium::Driver.new(app, :browser => :firefox)
 end
+
