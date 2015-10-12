f1bfca0c9863c4890a3a6047aca97453a11d8817
Nicolas Berrizbeitia <nbr625@gmail.com>
2015-10-12 13:07:19 -0400

    added create product and print specs

--- spec/features/add_print_spec.rb
+++ spec/features/add_print_spec.rb
@@ -1,5 +1,6 @@
 require './spec/spec_helper'
 require 'selenium-webdriver'
+require 'faker'
 
 feature "Create for a future print" do		
 
@@ -12,38 +13,53 @@ feature "Create for a future print" do
     	sleep(inspection_time=3)  
 	end	
 
-	it "should should not allow user to post review if comment is missing" do	
-		@driver.find_element(:id, 'reviewpath').click
-		@driver.navigate.refresh
-		sleep(inspection_time=3)  
-		@driver.find_element(:xpath, '/html/body/div[2]/form/div[1]/img[2]').click
+	it "should create a print" do	
+		@driver.find_element(:id, 'new_print').click
+		sleep(inspection_time=3)
+		# the following file path must be modified in your local environment
+		@driver.find_element(:id, 'print_image').send_keys "/Users/eduardokerese/projects/rspec_to_remote/spec/resources/flare_siren.jpg" 
+		print_title = Faker::Lorem.word
+		@driver.find_element(:id, 'name').send_keys print_title
+		@driver.find_element(:id, 'description').send_keys Faker::Lorem.paragraph
 		@driver.find_element(:name, 'commit').click
+		sleep(inspection_time=3)
+		title = @driver.find_element(:xpath, '/html/body/div[2]/div/div/div/div[1]/p[2]').text
+		expect(title).to eq(print_title)
+		
+	end
+
+	it "should let user their erase print" do
+		@driver.find_element(:id, 'eraseprintpath').click
 		sleep(inspection_time=3) 
-		alert = @driver.find_element(:xpath, '/html/body/div[2]/form/div[1]/ul/li').text
-		expect(alert).to eq("Comment can\'t be blank")
-		
+		notice = @driver.find_element(:id, 'notice').text
+		expect(notice).to eq("Print was successfully destroyed.")
 	end
 
-	it "should redirected me after finalizing pledge" do
-		page.attach_file('Image', 'specs/resources/flare_siren.jpg')
-		fill_in 'print[name]', :with => Faker::Name.name
-		fill_in 'print[description]', :with => 'Most Beautiful Image'
-		click_button "Create Print"
-		expect(page).to have_content('Most Beautiful Image')
+	it "should redirect user to print index after erasing" do
+		url = @driver.current_url
+		expect(url).to eq("https://artwear.herokuapp.com/prints")
 	end
 
-	it "should raise error if name is left black" do
-		
-		fill_in 'print[description]', :with => 'Most Beautiful Image'
-		click_button "Create Print"
-		expect(page).to have_content("Name can't be blank")
+	it "should raise error if name is left black" do	
+		@driver.find_element(:id, 'new_print').click
+		sleep(inspection_time=3)
+		@driver.find_element(:id, 'description').send_keys Faker::Lorem.paragraph
+		@driver.find_element(:name, 'commit').click
+		sleep(inspection_time=3)
+		alert = @driver.find_element(:xpath, '/html/body/div[2]/div/div/form/div[1]/ul/li').text
+		expect(alert).to eq("Name can't be blank")
 	end
 
 	it "should raise error if description is left black" do
-		
-		fill_in 'print[name]', :with => Faker::Name.name
-		click_button "Create Print"
-		expect(page).to have_content("Description can't be blank")
+		@driver.navigate.to "https://artwear.herokuapp.com/prints"
+		sleep(inspection_time=3)
+		@driver.find_element(:id, 'new_print').click
+		sleep(inspection_time=3)
+		@driver.find_element(:id, 'name').send_keys Faker::Lorem.word
+		@driver.find_element(:name, 'commit').click
+		sleep(inspection_time=3)
+		alert = @driver.find_element(:xpath, '/html/body/div[2]/div/div/form/div[1]/ul/li').text
+		expect(alert).to eq("Description can't be blank")
 	end
 
 	after(:all) do
