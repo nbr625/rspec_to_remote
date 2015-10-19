a7e894aafed7744eecab762caac243cc59b0362b
Nicolas Berrizbeitia <nbr625@gmail.com>
2015-10-18 22:18:28 -0400

    fixed last two specs to work with appium

--- spec/features/add_print_spec.rb
+++ spec/features/add_print_spec.rb
@@ -7,69 +7,82 @@ require './spec/environments'
 require_relative "../../spec/page_objects/home_page"
 require_relative "../../spec/page_objects/abstract_page"
 
-feature "Create for a future print" do		
+@environments.each do |env|
 
-	app = nil
+    feature "Signing in to ArtWare" do
 
-	before(:all) do
-	  	app = AbstractPage.new(Selenium::WebDriver.for(:firefox))
-	  	app.login
-	end
+      	app = nil
 
-	it "In #{env[:platform]} should create a print" do
-		print_title = Faker::Lorem.word
-		printTitle = app 
-			.navigateToAppRoot
-			.navigateToNewPrint
-			.inputImage
-			.inputName(print_title)
-			.inputDescription
-			.submitPrint
+    	if env[:type] == "computer"
 
-		title = printTitle.getPrintTitle
-		expect(title).to eq(print_title)
-		
-	end
+      		before(:all) do
+        		app = AbstractPage.new(Selenium::WebDriver.for(("#{env[:browser]}").to_sym))
+      		end
 
-	it "In #{env[:platform]} should let user delete their own print", "#{env[:name]}".to_sym do	
-		notice = app
-			.deletePrint
-			.getNoticeConfimation
+    	elsif env[:type] == "mobile"
 
-		expect(notice).to eq("Print was successfully destroyed.")
-	end
+      		before(:all) do
+        		appium_driver = Appium::Driver.new(env[:desired_caps])
+        		app = appium_driver.start_driver
+      		end
 
-	it "In #{env[:platform]} should redirect user to print index after erasing", "#{env[:name]}".to_sym do
-		url = app
-			.getUrl
+    	end
 
-		expect(url).to eq("https://artwear.herokuapp.com/prints")
-	end
+		it "In #{env[:platform]} should create a print" do
+			print_title = Faker::Lorem.word
+			printTitle = app 
+				.navigateToAppRoot
+				.navigateToNewPrint
+				.inputImage
+				.inputName(print_title)
+				.inputDescription
+				.submitPrint
 
-	it "In #{env[:platform]} should raise error if name is left blank", "#{env[:name]}".to_sym do	
-		printAlert = app 
-			.navigateToAppRoot
-			.navigateToNewPrint
-			.inputImage
-			.inputDescription
-			.submitInvalidPrint
-		alert = printAlert.getPrintAlert
-		expect(alert).to eq("Name can't be blank")
-	end
+			title = printTitle.getPrintTitle
+			expect(title).to eq(print_title)
+			
+		end
 
-	it "In #{env[:platform]} should raise error if description is left black", "#{env[:name]}".to_sym do
-		printAlert = app 
-			.navigateToAppRoot
-			.navigateToNewPrint
-			.inputImage
-			.inputName
-			.submitInvalidPrint
-		alert = printAlert.getPrintAlert
-		expect(alert).to eq("Description can't be blank")
-	end
+		it "In #{env[:platform]} should let user delete their own print", "#{env[:name]}".to_sym do	
+			notice = app
+				.deletePrint
+				.getNoticeConfimation
 
-	after(:all) do
-		app.quit
+			expect(notice).to eq("Print was successfully destroyed.")
+		end
+
+		it "In #{env[:platform]} should redirect user to print index after erasing", "#{env[:name]}".to_sym do
+			url = app
+				.getUrl
+
+			expect(url).to eq("https://artwear.herokuapp.com/prints")
+		end
+
+		it "In #{env[:platform]} should raise error if name is left blank", "#{env[:name]}".to_sym do	
+			printAlert = app 
+				.navigateToAppRoot
+				.navigateToNewPrint
+				.inputImage
+				.inputDescription
+				.submitInvalidPrint
+			alert = printAlert.getPrintAlert
+			expect(alert).to eq("Name can't be blank")
+		end
+
+		it "In #{env[:platform]} should raise error if description is left black", "#{env[:name]}".to_sym do
+			printAlert = app 
+				.navigateToAppRoot
+				.navigateToNewPrint
+				.inputImage
+				.inputName
+				.submitInvalidPrint
+			alert = printAlert.getPrintAlert
+			expect(alert).to eq("Description can't be blank")
+		end
+
+		after(:all) do
+			app.quit
+		end
 	end
 
 end
--- spec/features/add_review_to_print_spec.rb
+++ spec/features/add_review_to_print_spec.rb
@@ -8,67 +8,80 @@ require_relative "../../spec/page_objects/home_page"
 require_relative "../../spec/page_objects/abstract_page"
 require_relative "../../spec/page_objects/print_page"
 
-feature "Add reviews" do		
+@environments.each do |env|
 
-	app = nil
+    feature "Signing in to ArtWare" do
 
-	before(:all) do
-	  	app = AbstractPage.new(Selenium::WebDriver.for(:firefox))
-	  	app.login
+      	app = nil
+
+    	if env[:type] == "computer"
+
+      		before(:all) do
+        		app = AbstractPage.new(Selenium::WebDriver.for(("#{env[:browser]}").to_sym))
+      		end
+
+    	elsif env[:type] == "mobile"
+
+      		before(:all) do
+        		appium_driver = Appium::Driver.new(env[:desired_caps])
+        		app = appium_driver.start_driver
+      		end
+
+    	end
+
+
+		it "In #{env[:platform]} should allow user to post review" do
+			comment = Faker::Lorem.paragraph
+			newReview = app
+				.navigateToPrint 
+				.navigateToReview
+				.inputRating
+				.inputComment(comment)
+				.submitReview
+
+			new_review = newReview.getNewReview
+			expect(new_review).to eq(comment)
+			
+		end
+
+
+		it "In #{env[:platform]} should allow user to delete their comment and redirect to print", "#{env[:name]}".to_sym do
+			url = app
+				.deleteReview
+				.getUrl
+
+			expect(url).to eq("https://artwear.herokuapp.com/prints/17")
+		end
+
+
+
+		it "In #{env[:platform]} should not allow user to post review if comment is missing", "#{env[:name]}".to_sym do	
+			reviewMessage = app
+				.navigateToPrint 
+				.navigateToReview
+				.inputRating
+				.submitInvalidReview
+
+			alert = reviewMessage.reviewAlert
+			expect(alert).to eq("Comment can\'t be blank")		
+		end
+
+		it "In #{env[:platform]} should not allow user to post review if rating is missing", "#{env[:name]}".to_sym do
+			reviewMessage = app
+				.navigateToPrint 
+				.navigateToReview
+				.inputComment
+				.submitInvalidReview
+
+			alert = reviewMessage.reviewAlert
+			expect(alert).to eq("Rating can\'t be blank")
+		end
+
+
+		after(:all) do
+	    	app.quit
+	  	end
 	end
-
-
-	it "In #{env[:platform]} should allow user to post review" do
-		comment = Faker::Lorem.paragraph
-		newReview = app
-			.navigateToPrint 
-			.navigateToReview
-			.inputRating
-			.inputComment(comment)
-			.submitReview
-
-		new_review = newReview.getNewReview
-		expect(new_review).to eq(comment)
-		
-	end
-
-
-	it "In #{env[:platform]} should allow user to delete their comment and redirect to print", "#{env[:name]}".to_sym do
-		url = app
-			.deleteReview
-			.getUrl
-
-		expect(url).to eq("https://artwear.herokuapp.com/prints/17")
-	end
-
-
-
-	it "In #{env[:platform]} should not allow user to post review if comment is missing", "#{env[:name]}".to_sym do	
-		reviewMessage = app
-			.navigateToPrint 
-			.navigateToReview
-			.inputRating
-			.submitInvalidReview
-
-		alert = reviewMessage.reviewAlert
-		expect(alert).to eq("Comment can\'t be blank")		
-	end
-
-	it "In #{env[:platform]} should not allow user to post review if rating is missing", "#{env[:name]}".to_sym do
-		reviewMessage = app
-			.navigateToPrint 
-			.navigateToReview
-			.inputComment
-			.submitInvalidReview
-
-		alert = reviewMessage.reviewAlert
-		expect(alert).to eq("Rating can\'t be blank")
-	end
-
-
-	after(:all) do
-    	app.quit
-  	end
 
 end
 
